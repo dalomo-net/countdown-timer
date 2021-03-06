61323d354ac1f3c30b4e5b2935e82c99d30b97fd
PIERCE <etreraviauseptiemeciel@gmail.com>
2021-03-06 17:48:44 +0900

    .cancel() is not working, but that's enough. I'm tired

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -19,12 +19,15 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -34,6 +37,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.withTransform
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,24 +62,41 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
-    var buttonLabel = remember { mutableStateOf("START") }
-    var timeText = remember { mutableStateOf("1:00") }
+    val buttonLabel = remember { mutableStateOf("START") }
+    val timeText = remember { mutableStateOf("1:00") }
+    val num = remember { mutableStateOf(60000L) }
+    val finished = remember { mutableStateOf(true) }
 
     val countNum = object : CountDownTimer(60000, 1000) {
         override fun onTick(millisUntilFinished: Long) {
             timeText.value =
-                (millisUntilFinished / 1000 / 60).toString() + ":" + (millisUntilFinished / 1000 % 60).toString()
+                (millisUntilFinished / 1000 / 60).toString() + ":" + (millisUntilFinished / 1000 % 60).toString().padStart(2, '0')
+            num.value = millisUntilFinished
         }
 
         override fun onFinish() {
-            timeText.value = "FINISH..."
+            timeText.value = "FINISH"
+            finished.value = true
         }
     }
+
+    val t = if (timeText.value == "1:00") {
+        1f
+    } else {
+        (num.value - 1000f) / 60000f
+    }
+
     Surface(color = MaterialTheme.colors.background, modifier = Modifier.fillMaxSize()) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceEvenly
         ) {
+            Clock(
+                modifier = Modifier
+                    .aspectRatio(1.0f)
+                    .padding(64.dp),
+                t
+            )
             Text(
                 text = timeText.value,
                 fontSize = 60.sp,
@@ -80,24 +105,59 @@ fun MyApp() {
                     .padding(16.dp),
                 textAlign = TextAlign.Center
             )
-            Row(horizontalArrangement = Arrangement.SpaceEvenly) {
 
+            Row(horizontalArrangement = Arrangement.SpaceEvenly) {
                 Button(
                     onClick = {
-                        if (buttonLabel.value == "START") {
+                        if (buttonLabel.value == "START" && finished.value) {
                             countNum.start()
+                            finished.value = false
                             buttonLabel.value = "RESET"
                         } else {
                             countNum.cancel()
+                            finished.value = true
                             timeText.value = "1:00"
                             buttonLabel.value = "START"
+                            num.value = 60000L
                         }
-                    }
+                    },
+                    modifier = Modifier.width(100.dp)
                 ) {
                     Text(text = buttonLabel.value)
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Clock(modifier: Modifier = Modifier, t: Float) {
+
+    Canvas(modifier = modifier) {
+        val middle =
+            Offset(size.minDimension / 2, size.minDimension / 2)
+        drawCircle(
+            color = Color.Black,
+            center = middle,
+            radius = size.minDimension / 2,
+            style = Stroke(4.dp.toPx()),
+        )
+        withTransform(
+            {
+                rotate(360 * t, middle)
+            },
+            {
+                drawLine(
+                    strokeWidth = 8.dp.toPx(),
+                    cap = StrokeCap.Round,
+                    color = Color.Red,
+                    start = middle,
+                    end = Offset(size.minDimension / 2, 12.dp.toPx())
+                )
+            }
+        )
+
+        // To add drawing of hands later.
     }
 }
 
